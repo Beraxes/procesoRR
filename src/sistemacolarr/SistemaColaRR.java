6e351621594235fce762b9bb6a6d80cb98158e03
Javier Andres Olivera Soto <javierandresoliverasoto@gmail.com>
2022-07-02 09:53:31 -0500

    Implementando el metodo atender proceso

--- src/sistemacolarr/SistemaColaRR.java
+++ src/sistemacolarr/SistemaColaRR.java
@@ -19,18 +19,40 @@ public class SistemaColaRR {
         frente = null;
         cola = null;
     }
-    
-    public void colarProceso(){
+
+    public void colarProceso() {
         String n = JOptionPane.showInputDialog("Ingrese nombre de proceso: ");
         int ti = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tiempo estimado de ejecucion: "));
         if (frente == null) {
             frente = new proceso();
             frente.registrarProceso(n, ti);
             cola = frente;
-        }else if (frente !=null) {
+        } else if (frente != null) {
             proceso tmp = new proceso();
             tmp.registrarProceso(n, ti);
+            cola.setSiguiente(tmp);
             cola = tmp;
+        }
+    }
+
+    public void atenderProceso() {
+        int tiempoPermitido = 10;
+        proceso temporal = frente, aux;
+        if (temporal != null) {
+            if (temporal.et > 0) {
+                temporal.mostrarProc();
+                temporal.setEt(temporal.getEt() - tiempoPermitido);
+                if (temporal.et > 0) {
+                    cola.setSiguiente(temporal);
+                    cola = temporal;
+                } else {
+                    aux = temporal;
+                    temporal = null;
+                    temporal = aux.getSiguiente();
+
+                }
+
+            }
         }
     }
 
